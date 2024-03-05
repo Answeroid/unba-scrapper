f732e2a5a4ccb5f5f2fb56b0540bd15eb1329865
Answeroid <send.mail.to.flash@gmail.com>
2024-03-05 11:11:53 +0200

    edit runner script to properly work out of the box

--- unba/run_spider.sh
+++ unba/run_spider.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 while true; do
-  read -r -p "\nPlease provide spider name: " name
+  read -r -p "Please provide spider name: " name
   if [[ -n "$name" ]] ; then
-    scrapy crawl "$name" -O res/"$name".csv -O res/"$name".json
+    # Check if the spider exists
+    if scrapy list | grep -q "$name"; then
+      # Spider exists, run it
+      scrapy crawl "$name" -O res/"$name".csv -O res/"$name".json | tee res/crawl.log
+      break
+    else
+      echo "Error: Spider '$name' not found. Please enter a valid spider name."
+    fi
   else
-    echo "Please respond correctly."
+    echo "Error: Please enter a valid spider name."
   fi
 done
